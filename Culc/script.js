c971fda81bb6ebe9ffd3282c071c4d21fa7f099a
ElDanto <pavlovian.ch@gmail.com>
2018-07-28 20:37:40 +0300

    Consol culc

--- Culc/script.js
+++ Culc/script.js
@@ -1,11 +1,25 @@
-// var a,b,res;
-//
-// a+b=res;
-// a-b=res;
-// a/b=res;
-// a*b=res;
-// a%b=res;
+var x , y , op , res;
 console.log(111);
-function culc() {
-    return 10;
+function culc(x,y,op) {
+    if(op = '+'){
+        res= x + y;
+        return res;
+    }else if(op = '-'){
+        res= x - y;
+        return res;
+    }else if(op = '*'){
+        res= x * y;
+        return res;
+    }else if(op = '/'){
+        res= x / y;
+        return res;
+    }else if(op = '+'){
+        res= x + y;
+        return res;
+    }else if(op = 'mod'){
+        res= x % y;
+        return res;
+    }else{
+        return 'Eror NaN'
+    }
 }
