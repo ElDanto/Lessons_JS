28b52a00b14ff19a1160b6cbe3b46bd40a03010c
ElDanto <pavlovian.ch@gmail.com>
2018-08-07 17:47:39 +0300

    Lesson_3_Homework

---
+++ Homework/Lesson_3/Lesson_3_IF.js
@@ -0,0 +1,48 @@
+/*
+if ("0") {
+    alert( 'Привет' );
+}
+*/
+/*
+var ans = 'ECMAScript';//Пусть ответ будет  записываться в эту переменную
+if(ans == 'ECMAScript'){
+    alert('Верно!');
+}else {
+    alert('Не знаете? «ECMAScript»!')
+}*/
+/*
+var result = prompt('Введите число', 0);
+
+if (result > 0) {
+    alert( 1 );
+} else if (result < 0) {
+    alert( -1 );
+} else {
+    alert( 0 );
+}
+*/
+
+/*
+var userName = prompt('Кто пришёл?', '');
+
+if (userName == 'Админ') {
+
+    var pass = prompt('Пароль?', '');
+
+    if (pass == 'Чёрный Властелин') {
+        alert( 'Добро пожаловать!' );
+    } else if (pass == null) { // (*)
+        alert( 'Вход отменён' );
+    } else {
+        alert( 'Пароль неверен' );
+    }
+
+} else if (userName == null) { // (**)
+    alert( 'Вход отменён' );
+
+} else {
+
+    alert( 'Я вас не знаю' );
+
+}
+*/
